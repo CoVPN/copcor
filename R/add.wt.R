6d40d5417185f1b917403f8d0daf12f42f5386ec
Youyi Fong <youyifong@gmail.com>
2024-03-26 19:55:03 -0700

    updates

--- R/add.wt.R
+++ R/add.wt.R
@@ -5,7 +5,7 @@ add.wt=function(dat, ph1="ph1", ph2="ph2", Wstratum="Wstratum", wt="wt", verbose
   if(verbose) print(wts_table)
   wts_norm <- rowSums(wts_table) / wts_table[, 2]
   dat[[wt]] <- wts_norm[dat[[Wstratum]] %.% ""]
-  dat[ph1!=1, wt] = NA   # the step above assigns weights for some subjects outside ph1. the next step makes them NA
+  dat[dat[[ph1]]!=1, wt] = NA   # the step above assigns weights for some subjects outside ph1. the next step makes them NA
   dat
 
 }
